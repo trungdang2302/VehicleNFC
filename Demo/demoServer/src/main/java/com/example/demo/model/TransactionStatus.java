86043e9af7ee8595df7bc9ff40190cb17d5bab1c
Baonq1997 <baonq1997@gmail.com>
2018-09-23 22:52:23 +0700

    Baonq: Create Entities

---
+++ Demo/demoServer/src/main/java/com/example/demo/model/TransactionStatus.java
@@ -0,0 +1,102 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.demo.model;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author BaoNQ
+ */
+@Entity
+@Table(name = "tbl_transaction_status")
+@XmlRootElement
+public class TransactionStatus implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    @Size(max = 45)
+    @Column(name = "name")
+    private String name;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "transactionStatusId")
+    private List<Transaction> transactionList;
+
+    public TransactionStatus() {
+    }
+
+    public TransactionStatus(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @XmlTransient
+    public List<Transaction> getTransactionList() {
+        return transactionList;
+    }
+
+    public void setTransactionList(List<Transaction> transactionList) {
+        this.transactionList = transactionList;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof TransactionStatus)) {
+            return false;
+        }
+        TransactionStatus other = (TransactionStatus) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "sample.model.TransactionStatus[ id=" + id + " ]";
+    }
+    
+}
--- Demo/demoServer/src/main/java/com/example/demo/model/User.java
+++ Demo/demoServer/src/main/java/com/example/demo/model/User.java
@@ -1,37 +1,144 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package com.example.demo.model;
 
-import javax.persistence.*;
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
 
+/**
+ *
+ * @author BaoNQ
+ */
 @Entity
 @Table(name = "tbl_user")
-public class User {
-    private int id;
-    private String phone;
-    private String vehicleNumber;
-
+@XmlRootElement
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
     @Column(name = "id")
-    public int getId() {
-        return id;
+    private Integer id;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 15)
+    @Column(name = "phone_number")
+    private String phoneNumber;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 60)
+    @Column(name = "password")
+    private String password;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "money")
+    private int money;
+    @Size(max = 45)
+    @Column(name = "first_name")
+    private String firstName;
+    @Size(max = 45)
+    @Column(name = "last_name")
+    private String lastName;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 10)
+    @Column(name = "vehicle_number")
+    private String vehicleNumber;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 10)
+    @Column(name = "license_plate_id")
+    private String licensePlateId;
+    @JoinColumn(name = "tbl_vehicle_type_id", referencedColumnName = "id")
+    @ManyToOne(optional = false)
+    private VehicleType vehicleTypeId;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "userId")
+    private List<Transaction> transactionList;
+
+    public User() {
     }
 
-    public void setId(int id) {
+    public User(Integer id) {
         this.id = id;
     }
 
-    @Basic
-    @Column(name = "phone_number")
-    public String getPhone() {
-        return phone;
+    public User(Integer id, String phoneNumber, String password, int money, String vehicleNumber, String licensePlateId) {
+        this.id = id;
+        this.phoneNumber = phoneNumber;
+        this.password = password;
+        this.money = money;
+        this.vehicleNumber = vehicleNumber;
+        this.licensePlateId = licensePlateId;
     }
 
-    public void setPhone(String phone) {
-        this.phone = phone;
+    public Integer getId() {
+        return id;
     }
 
-    @Basic
-    @Column(name = "vehicle_number")
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public void setMoney(int money) {
+        this.money = money;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
     public String getVehicleNumber() {
         return vehicleNumber;
     }
@@ -39,4 +146,55 @@ public class User {
     public void setVehicleNumber(String vehicleNumber) {
         this.vehicleNumber = vehicleNumber;
     }
+
+    public String getLicensePlateId() {
+        return licensePlateId;
+    }
+
+    public void setLicensePlateId(String licensePlateId) {
+        this.licensePlateId = licensePlateId;
+    }
+
+//    public VehicleType getVehicleTypeId() {
+//        return vehicleTypeId;
+//    }
+//
+//    public void setVehicleTypeId(VehicleType vehicleTypeId) {
+//        this.vehicleTypeId = vehicleTypeId;
+//    }
+//
+//    @XmlTransient
+//    public List<Transaction> getTransactionList() {
+//        return transactionList;
+//    }
+//
+//    public void setTransactionList(List<Transaction> transactionList) {
+//        this.transactionList = transactionList;
+//    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof User)) {
+            return false;
+        }
+        User other = (User) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "sample.model.User[ id=" + id + " ]";
+    }
+    
 }

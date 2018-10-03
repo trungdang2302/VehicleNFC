ebbe29e431654f8eb58c0646d6fd105932c6323a
swomfire <vucuongJim@gmail.com>
2018-10-03 21:53:26 +0700

    add creteria

--- Demo/demoServer/src/main/java/com/example/demo/service/UserService.java
+++ Demo/demoServer/src/main/java/com/example/demo/service/UserService.java
@@ -1,12 +1,20 @@
 package com.example.demo.service;
 
+import com.example.demo.Config.SearchCriteria;
 import com.example.demo.model.User;
 import com.example.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityManager;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import java.nio.ByteBuffer;
+import java.util.ArrayList;
 import java.util.Base64;
 import java.util.List;
 import java.util.Optional;
@@ -42,6 +50,9 @@ public class UserService {
     }
 
     public Page<User> getAllUser(Integer page, Integer numOfRows) {
+        List<SearchCriteria> params = new ArrayList<>();
+        params.add(new SearchCriteria("lastName", ":", "mai"));
+        searchUser(params);
         return userRepository.findAll(new PageRequest(page, numOfRows));
     }
 
@@ -54,6 +65,42 @@ public class UserService {
         b.putInt(id);
         byte[] result = b.array();
         return Base64.getEncoder().withoutPadding().encodeToString(result);
+    }
+
+    @Autowired
+    private EntityManager entityManager;
+
+    public List<User> searchUser(List<SearchCriteria> params) {
+        CriteriaBuilder builder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<User> query = builder.createQuery(User.class);
+        Root r = query.from(User.class);
+
+        Predicate predicate = builder.conjunction();
+
+        for (SearchCriteria param : params) {
+            if (param.getOperation().equalsIgnoreCase(">")) {
+                predicate = builder.and(predicate,
+                        builder.greaterThanOrEqualTo(r.get(param.getKey()),
+                                param.getValue().toString()));
+            } else if (param.getOperation().equalsIgnoreCase("<")) {
+                predicate = builder.and(predicate,
+                        builder.lessThanOrEqualTo(r.get(param.getKey()),
+                                param.getValue().toString()));
+            } else if (param.getOperation().equalsIgnoreCase(":")) {
+                if (r.get(param.getKey()).getJavaType() == String.class) {
+                    predicate = builder.and(predicate,
+                            builder.like(r.get(param.getKey()),
+                                    "%" + param.getValue() + "%"));
+                } else {
+                    predicate = builder.and(predicate,
+                            builder.equal(r.get(param.getKey()), param.getValue()));
+                }
+            }
+        }
+        query.where(predicate);
+
+        List<User> result = entityManager.createQuery(query).getResultList();
+        return result;
     }
 
 }
